45820ea511ba4b7f111a6bba3f6d48e2705cf000
luizgfalqueto <lgfalqueto85@gmail.com>
2024-03-26 22:51:19 -0300

    feat: fill pre-checkin with data and button actions

--- lib/src/pages/pre_checkin/pre_checkin_page.dart
+++ lib/src/pages/pre_checkin/pre_checkin_page.dart
@@ -1,14 +1,162 @@
+import 'package:fe_lab_clinicas_adm/src/models/patient_information_form_model.dart';
+import 'package:fe_lab_clinicas_adm/src/pages/pre_checkin/pre_checkin_controller.dart';
+import 'package:fe_lab_clinicas_adm/src/shared/data_item.dart';
+import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 
-class PreCheckinPage extends StatelessWidget {
+class PreCheckinPage extends StatefulWidget {
+  const PreCheckinPage({super.key});
 
-  const PreCheckinPage({ super.key });
+  @override
+  State<PreCheckinPage> createState() => _PreCheckinPageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Pre Checkin'),),
-           body: Container(),
-       );
+class _PreCheckinPageState extends State<PreCheckinPage> with MessageViewMixin {
+  final controller = Injector.get<PreCheckinController>();
+
+  @override
+  void initState() {
+    messageListener(controller);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final PatientInformationFormModel(:password, :patient) =
+        controller.informationForm.watch(context)!;
+
+    var sizeOf = MediaQuery.sizeOf(context);
+    return Scaffold(
+      appBar: LabClinicasAppBar(),
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: sizeOf.width * .5,
+            margin: const EdgeInsets.only(top: 56),
+            padding: const EdgeInsets.all(40),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(color: LabClinicasTheme.orangeColor),
+            ),
+            child: Column(
+              children: [
+                Image.asset('assets/images/patient_avatar.png'),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text(
+                  'A senha chamada foi',
+                  style: LabClinicasTheme.titleSmallStyle,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  height: 48,
+                  decoration: BoxDecoration(
+                    color: LabClinicasTheme.orangeColor,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        password,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 48,
+                ),
+                DataItem(
+                  label: 'Nome paciente',
+                  value: patient.name,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Email',
+                  value: patient.email,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Telefone de COntato',
+                  value: patient.phoneNumber,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'CPF',
+                  value: patient.document,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'CEP',
+                  value: patient.address.cep,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Endereço',
+                  value:
+                      '${patient.address.streetAddress}, ${patient.address.number},'
+                      '${patient.address.addressComplement},${patient.address.district},'
+                      '${patient.address.city} - ${patient.address.state}',
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Responsável',
+                  value: patient.guardian,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                DataItem(
+                  label: 'Documento de Identificação',
+                  value: patient.guardianIdentificationNumber,
+                  padding: const EdgeInsets.only(bottom: 24),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: OutlinedButton(
+                        onPressed: () async {
+                          await controller.next();
+                        },
+                        child: const Text('CHAMAR OUTRA SENHA'),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.of(context).pushReplacementNamed(
+                            '/checkin',
+                            arguments: controller.informationForm,
+                          );
+                        },
+                        child: const Text('ATENDER'),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
